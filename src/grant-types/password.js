6d995947827497960a42cfa874f2a66aba70e2e0
cw <789@webuhu.at>
2018-07-13 09:55:20 +0200

    fix: bcrypt never missmatches

--- src/grant-types/password.js
+++ src/grant-types/password.js
@@ -35,41 +35,36 @@ export default function({
     return Promise.reject(new HTTPError(OAUTH_PASSWORD_MISSING));
   }
 
-  return userModel.findOne({ where: { [usernameField]: username } }).then((user) => {
-    if (!user) {
-      return Promise.reject(new HTTPError(OAUTH_INVALID_CREDENTIALS));
-    }
+  return userModel.findOne({ where: { [usernameField]: username } }).then(async (user) => {
+    if (!user) return Promise.reject(new HTTPError(OAUTH_INVALID_CREDENTIALS));
 
-    return bcrypt
-      .compare(password, user[passwordField])
-      .then(() => {
-        const accessToken = tokenModel.create({
-          type: TOKEN_TYPE_ACCESS,
-          expires_at: moment()
-            .add(accessTokenTTL, 'hours')
-            .format(),
-          ownerId: user.id
-        });
-        const refreshToken = tokenModel.create({
-          type: TOKEN_TYPE_REFRESH,
-          expires_at: moment()
-            .add(refreshTokenTTL, 'days')
-            .format(),
-          ownerId: user.id
-        });
+    const match = await bcrypt.compare(password, user[passwordField])
+    if (!match) return Promise.reject(new HTTPError(OAUTH_INVALID_PASSWORD))
 
-        return Promise.all([accessToken, refreshToken]).then((tokens) => {
-          const [accessToken, refreshToken] = tokens;
-          res.body = {
-            access_token: accessToken[tokenField],
-            refresh_token: refreshToken[tokenField],
-            token_type: ACCESS_TOKEN_TYPE_BEARER,
-            expires_in: moment(accessToken.expires_at).diff(moment(), 'seconds'),
-            user_id: user.id
-          };
-        });
-      })
-      .catch(bcrypt.MISMATCH_ERROR, () => Promise.reject(new HTTPError(OAUTH_INVALID_PASSWORD)))
-      .catch(() => Promise.reject(new HTTPError(OAUTH_INVALID_CREDENTIALS)));
+    const accessToken = tokenModel.create({
+      type: TOKEN_TYPE_ACCESS,
+      expires_at: moment()
+        .add(accessTokenTTL, 'hours')
+        .format(),
+      ownerId: user.id
+    });
+    const refreshToken = tokenModel.create({
+      type: TOKEN_TYPE_REFRESH,
+      expires_at: moment()
+        .add(refreshTokenTTL, 'days')
+        .format(),
+      ownerId: user.id
+    });
+
+    return Promise.all([accessToken, refreshToken]).then((tokens) => {
+      const [accessToken, refreshToken] = tokens;
+      res.body = {
+        access_token: accessToken[tokenField],
+        refresh_token: refreshToken[tokenField],
+        token_type: ACCESS_TOKEN_TYPE_BEARER,
+        expires_in: moment(accessToken.expires_at).diff(moment(), 'seconds'),
+        user_id: user.id
+      };
+    });
   });
 }
